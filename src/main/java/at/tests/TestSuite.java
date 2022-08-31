6212bfe96cf5f341fd9b1e618f1d337152210e9c
alexsivochka <36335410+alexsivochka@users.noreply.github.com>
2022-08-31 15:59:33 +0300

    Update TestSuite.java

--- src/main/java/at/tests/TestSuite.java
+++ src/main/java/at/tests/TestSuite.java
@@ -21,44 +21,4 @@ public class TestSuite {
         Assert.assertTrue(true);
     }
 
-    @Test(priority = 20, description = "Этот тест должен был упасть")
-    public void testShouldFail() {
-        Assert.assertTrue(false);
-    }
-
-    @Test(priority = 30, description = "Этот тест должен быть пропущен", dependsOnMethods = "testShouldFail")
-    public void testShouldSkip() {
-        Assert.assertTrue(false);
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 1")
-    public void testShouldFail1() {
-        Assert.assertTrue(false);
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 2")
-    public void testShouldFail2() {
-        Assert.assertEquals("Первый текст", "Второй текст не равен первому");
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 3")
-    public void testShouldFail3() {
-        Assert.assertTrue(false);
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 4")
-    public void testShouldFail4() {
-        Assert.assertTrue(false);
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 5")
-    public void testShouldFail5() {
-        Assert.assertEquals("Первый текст", "Второй текст не равен первому");
-    }
-
-    @Test(priority = 20, description = "Этот тест должен был упасть 6")
-    public void testShouldFail6() {
-        Assert.assertTrue(false);
-    }
-
 }
